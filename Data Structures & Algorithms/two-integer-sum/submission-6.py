3a621f786888000bd07982aef2732ea40d63c3c6
jroxasqq <111217654+jroxasqq@users.noreply.github.com>
2026-08-08 22:11:32 +1000

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # the partner to nums[i] is target - nums[i].
+        partners = dict()
+        for i in range(len(nums)):
+            partner = target - nums[i]
+            if partner in partners:
+                return [partners[partner], i]
+            partners[nums[i]] = i
+        
+        raise RuntimeError("one pair of indices was assumed to exist.")
